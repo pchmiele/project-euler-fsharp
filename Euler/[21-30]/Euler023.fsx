8090c095096eb4953dce28cb18f7356250d0e8d1
pchmiele <przemyslaw.chmielewski.gda@gmail.com>
2016-05-02 06:43:54 +0200

    Added solution of 23th euler problem

--- Euler/[21-30]/Euler023.fsx
+++ Euler/[21-30]/Euler023.fsx
@@ -1,34 +1,29 @@
 let euler23 =
+    let max = 28123
+    
     let properDivisors (n:int) =
         [1..n/2]
-        |> Seq.filter (fun x -> n % x = 0)
+        |> List.filter (fun x -> n % x = 0)
    
     let isAbundantNumber n = 
-        let sumOfDivisors = (properDivisors n)|> Seq.sum
+        let sumOfDivisors = (properDivisors n)|> List.sum
         sumOfDivisors > n
-
-    let isNonAbundantNumber n = 
-        not (isAbundantNumber n)
  
     let allAbundantNumbers =
-        [1..100]
-        |> Seq.filter isAbundantNumber
+        [1..max] |> List.filter isAbundantNumber
 
-    let nonAbundantNumbers = 
-        [1..100]
-        |> Seq.filter isNonAbundantNumber
-
-    let canBePresentedAsSumOfAbundantNumbers x = 
-        let part = 1..
+    let allSumsOf2AbundantNumbers = 
+        allAbundantNumbers
+        |> List.collect (fun x -> allAbundantNumbers |> List.map (fun y -> x + y))
+        |> List.filter (fun x -> x <= max)
+        |> List.distinct
         
-        x
-        |>
     let result = 
-        nonAbundantNumbers 
-        |> Seq.map (fun x -> x)
+        let sumOfNumbers = [1..max] |> List.sum
+        let sumOfSumsOf2AbundatNumbers = allSumsOf2AbundantNumbers |> List.sum
 
+        sumOfNumbers - sumOfSumsOf2AbundatNumbers
+        
+    result
 
-    printfn "%d" (allAbundantNumbers |> Seq.length) 
-    printfn "%d" (nonAbundantNumbers |> Seq.length) 
-
-printfn "%A" euler23
+printfn "%d" euler23
