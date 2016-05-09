599aa22accf5958012f8cdd53e643a9d21471432
pchmiele <przemyslaw.chmielewski.gda@gmail.com>
2016-05-09 12:47:15 +0200

    Cleanup of 47th project euler problem

---
+++ Euler/[41-50]/Euler047.fsx
@@ -0,0 +1,21 @@
+﻿let euler48 =
+    let numbers = [1I..1000I]
+    
+    let selfPower (n:bigint) =
+        pown n (int32 n)
+    
+    let selfPowers = 
+        numbers
+        |> Seq.map selfPower
+        |> Seq.sum
+        
+    let result = 
+        selfPowers.ToString().ToCharArray()
+        |> Seq.rev
+        |> Seq.take 10
+        |> Seq.rev
+        |> Seq.map string
+            
+    String.concat "" result
+    
+printfn "%A" euler48
