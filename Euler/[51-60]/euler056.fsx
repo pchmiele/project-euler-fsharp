285e0db7dbb0929c3849dc5147bed8c74ee77b55
pchmiele <przemyslaw.chmielewski.gda@gmail.com>
2016-05-12 17:11:51 +0200

    Added solution of 56th project euler problem

---
+++ Euler/[51-60]/euler056.fsx
@@ -0,0 +1,17 @@
+let euler56 =  
+    let numbers = [1..99]
+    
+    let digitsSum n =
+        n.ToString().ToCharArray() 
+        |> Array.toList
+        |> List.map (fun x -> (bigint.Parse(string x)))
+        |> List.sum
+    
+    let result = 
+        numbers 
+        |> Seq.collect(fun x -> numbers |> Seq.map (fun y -> pown (bigint x) (y)))
+        |> Seq.map digitsSum
+        |> Seq.max
+  
+    result
+printfn "%A" euler56
